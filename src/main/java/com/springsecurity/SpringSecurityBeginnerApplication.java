29c4a4656aa2bb733dac05a8c40e399fb6180e4b
유석현 <shyoo1023@cellbig.com>
2025-08-13 15:56:25 +0900

    INIT spring security project

---
+++ src/main/java/com/springsecurity/SpringSecurityBeginnerApplication.java
@@ -0,0 +1,11 @@
+package com.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBeginnerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBeginnerApplication.class, args);
+    }
+}
