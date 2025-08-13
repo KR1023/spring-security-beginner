252db8faa97dca27ddf8c8af990800cf5d9f4242
유석현 <shyoo1023@cellbig.com>
2025-08-13 17:05:58 +0900

    UPDATE SecurityConfig. 메모리 사용자 방식으로 사용자 인증

--- src/main/java/com/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/springsecurity/config/SecurityConfig.java
@@ -3,10 +3,41 @@ package com.springsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    // 비밀번호 암호화 Bean 등록
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        // BCryptPasswordEncoder: 해시 기반 암호화, 복호화 불가능(보안에 안전)
+        return new BCryptPasswordEncoder();
+    }
+
+    // 메모리 기반 사용자 저장소 Bean
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails user = User.withUsername("user")
+                .password(encoder.encode("1234"))    // 비밀번호 암호화 필수
+                .roles("USER")  // ROLE_USER
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(encoder.encode("1234"))
+                .roles("ADMIN") // ROLE_ADMIN
+                .build();
+
+        // InMemoryUserDetailsManager: 메모리에 사용자 저장
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -19,6 +50,7 @@ public class SecurityConfig {
                             "/css/**",
                             "/js/**",
                             "/images/**").permitAll()   // permitAll(): 인증 없이 누구나 접근 가능
+                    .requestMatchers("/admin/**").hasRole("ADMIN")  // ROLE_ADMIN 필요
                     .anyRequest().authenticated()   // 그 외 모든 요청은 인증 필요
             )
             // 폼 로그인 설정
